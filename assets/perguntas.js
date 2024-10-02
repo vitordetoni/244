6ac00f580b64a38d441402b4be6df32beb3c686f
vitordetoni <162642177+vitordetoni@users.noreply.github.com>
2024-10-02 19:35:49 -0300

    Add files via upload

---
+++ assets/perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Programação',
+    'O que é uma variavel em programação?',
+    'E um espaço na memoria do computador'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital da Brasil e Brasilia'
+)
+
+criaCartao(
+    'Programação',
+    'Quais são os 4 pilares da programação?',
+    'Abstração, encapsulamento, herança, polimorfismo'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em ingles?',
+    'Oi em ingles é hey'
+)
+
+criaCartao(
+    'lingua espanhola',
+    'Como se fala lanche em espanhol?',
+    'Se fala merienda'
+)  
+
+criaCartao( 
+    'Geografia',    
+    'Qual a idade da terra',
+    'Aproximadamente 4,3 bilhões de anos'
+
+)
+criaCartao(
+    'Linguagens',
+    'Como se fala teclado em ingles',
+    'Teclado em ingles e keyboard'
+    
+)
+criaCartao(
+    'Linguagens',
+    'Como se fala tijolo em ingles',
+    'Tijolo em ingles se fala brick'
+
+)
+criaCartao(
+    'Mecanica',
+    'Qual oleo se usa em motores 4 tempos',
+    'Se usa o oleo 10W-30'
+
+)
+criaCartao(
+    'Mecanica',
+    'Qual oleo se usa em motores 2 tempos',
+    'Se usa oleo 75W-90'
+)
